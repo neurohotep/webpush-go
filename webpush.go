c30a823f3536630f4a01024778b7095653b20b32
Dias Umertasov <umertasov.d@gmail.com>
2019-10-30 16:13:25 +0300

    Test

--- webpush.go
+++ webpush.go
@@ -19,21 +19,6 @@ import (
 	"golang.org/x/crypto/hkdf"
 )
 
-// Urgency indicates to the push service how important a message is to the user.
-// This can be used by the push service to help conserve the battery life of a user's device
-// by only waking up for important messages when battery is low.
-type Urgency string
-
-const (
-	// UrgencyVeryLow requires device state: on power and Wi-Fi
-	UrgencyVeryLow Urgency = "very-low"
-	// UrgencyLow requires device state: on either power or Wi-Fi
-	UrgencyLow Urgency = "low"
-	// UrgencyNormal excludes device state: low battery
-	UrgencyNormal Urgency = "normal"
-	// UrgencyHigh admits device state: low battery
-	UrgencyHigh Urgency = "high"
-)
 
 var saltFunc = func() ([]byte, error) {
 	salt := make([]byte, 16)
@@ -259,11 +244,3 @@ func getInfo(infoType, clientPublicKey, serverPublicKey []byte) []byte {
 	return info.Bytes()
 }
 
-// Checking allowable values for the urgency header
-func isValidUrgency(urgency Urgency) bool {
-	switch urgency {
-	case UrgencyVeryLow, UrgencyLow, UrgencyNormal, UrgencyHigh:
-		return true
-	}
-	return false
-}
